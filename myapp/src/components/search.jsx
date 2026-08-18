efcc4f63a6c8d7ee486faaed9dd09628d2acdbe6
Kishore <kingofthehills32@gmail.com>
2026-08-18 19:25:01 +0530

    fix:made the website fully respsoive

--- myapp/src/components/search.jsx
+++ myapp/src/components/search.jsx
@@ -3,15 +3,16 @@
 const Search = ({ searchTerm, setSearchTerm }) => {
   return (
     <div className="search">
-      <img src="./search.svg" alt="search" />
-     
-      <input
-      
-        type="text"
-        placeholder="Search your favourite movie"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+      <div>
+        <img src="./search.svg" alt="search" />
+
+        <input
+          type="text"
+          placeholder="Search your favourite movie"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
     </div>
   );
 };
